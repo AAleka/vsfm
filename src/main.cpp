b268e556b1a048f33ff5ed56d4fff3acf6371a9b
AAleka <mr.allnur.55@gmail.com>
2023-12-06 16:25:50 +0300

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
